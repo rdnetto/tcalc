bfec3b9a9ca0e20f4f391132458f204f304ed55a
Reuben D'Netto <rdnetto@gmail.com>
2018-08-15 18:31:30 +1000

    Minimal repro for GHC panic

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,6 @@
+{-# LANGUAGE StrictData #-}
+
 module Main where
 
-import BasicPrelude
-import Text.Megaparsec (runParser)
-import Text.Megaparsec.Error (parseErrorPretty)
-
-import Parser
-
-
-main :: IO ()
-main = mapM evalLine . map lines $ getContents
-
-evalLine :: LText -> IO ()
-evalLine txt = do
-    res <- runParser "<stdin>" exprParser txt
-    case res of
-         Right ast -> evalAST ast
-         Left  err -> P.putStrLn $ parseErrorPretty err
+newtype Duration = Foo
+data Literal = LitDuration Duration
